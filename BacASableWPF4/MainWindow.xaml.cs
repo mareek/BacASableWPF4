34152c34c4ee6b9a449cd8ba189b0db3a3f9c062
Matthieu Mourisson <mareek@gmail.com>
2015-01-22 17:20:42 +0100

    Type generation helpers

--- BacASableWPF4/MainWindow.xaml.cs
+++ BacASableWPF4/MainWindow.xaml.cs
@@ -32,6 +32,7 @@ using MongoDB.Bson.Serialization;
 using MongoDB.Bson.IO;
 using Microsoft.Web.XmlTransform;
 using System.Net.Http;
+using System.Reflection;
 
 namespace BacASableWPF4
 {
@@ -49,8 +50,125 @@ namespace BacASableWPF4
 
         private void TestButton_Click(object sender, RoutedEventArgs e)
         {
-            Console.Write(GenerateGuidList(286));
+            Console.Write(GenerateFunctionCodeForAssembly(typeof(MainWindow)));
         }
+
+        public static string GenerateFunctionCodeForAssembly(Type type)
+        {
+            var namespaceTypes = type.Assembly.GetTypes().Where(t => t.Namespace == type.Namespace);
+
+            return string.Join("\n", namespaceTypes.Select(GenerateCodeInitialisation));
+        }
+
+        private static string GenerateCodeInitialisation(Type type)
+        {
+            const string indentation = "    ";
+
+            var declarationBuilder = new StringBuilder();
+            declarationBuilder.AppendLine("public static " + type.Name + " Generate" + type.Name + "()");
+            declarationBuilder.AppendLine("{");
+            declarationBuilder.AppendLine(indentation + "return new " + type.Name);
+            declarationBuilder.AppendLine(indentation + "{");
+
+            var properties = type.GetProperties(BindingFlags.Instance | BindingFlags.Public);
+            foreach (var property in properties.Where(p => p.CanWrite))
+            {
+                declarationBuilder.AppendLine(indentation + indentation + GeneratePropertySetter(property));
+            }
+
+            declarationBuilder.AppendLine(indentation + "};");
+            declarationBuilder.AppendLine("}");
+
+            return string.Join("\n", declarationBuilder);
+        }
+
+        private static string GeneratePropertySetter(PropertyInfo property)
+        {
+            var declarationBuilder = new StringBuilder();
+
+            declarationBuilder.Append(property.Name);
+            declarationBuilder.Append(" = ");
+            declarationBuilder.Append(GeneratePropertyDefaultValue(property));
+            declarationBuilder.Append(",");
+
+            return declarationBuilder.ToString();
+        }
+
+        private static string GeneratePropertyDefaultValue(PropertyInfo property)
+        {
+            if (property.PropertyType == typeof(string))
+            {
+                return "\"" + property.Name + "\"";
+            }
+            else
+            {
+                return GenerateTypeDefaultValue(property.PropertyType);
+            }
+        }
+
+        private static string GenerateTypeDefaultValue(Type type)
+        {
+            switch (type.FullName)
+            {
+                case "System.Boolean":
+                    return "true";
+                case "System.DateTime":
+                    return "DateTime.Today";
+                case "System.Int16":
+                case "System.Int32":
+                case "System.Int64":
+                case "System.UInt16":
+                case "System.UInt32":
+                case "System.UInt64":
+                case "System.Decimal":
+                case "System.Single":
+                case "System.Double":
+                case "System.Byte":
+                case "System.SByte":
+                    return "0";
+                default:
+                    return GenerateSpecialTypesDefaultValue(type);
+            }
+        }
+
+        private static string GenerateSpecialTypesDefaultValue(Type type)
+        {
+            Func<Type, bool> isIList = t => t.Name.StartsWith("IList") || t.GetInterfaces().Any(i => i.Name.StartsWith("IList"));
+
+            if (type.IsEnum)
+            {
+                return type.Name + "." + Enum.GetValues(type).Cast<object>().First().ToString();
+            }
+            else if (type.IsGenericType && type.Name.StartsWith("Nullable"))
+            {
+                return GenerateTypeDefaultValue(type.GenericTypeArguments[0]);
+            }
+            else if (type.IsGenericType && isIList(type))
+            {
+                return "new[] { " + GenerateTypeDefaultValue(type.GenericTypeArguments[0]) + " }";
+            }
+            else
+            {
+                return "Generate" + type.Name + "()";
+            }
+        }
+
+        private string GetFullName(Type type)
+        {
+            if (type.IsGenericType)
+            {
+                var cleanName = type.Name.Split('`').First();
+                var namespacedName = type.Namespace + "." + cleanName;
+                var genericParameters = string.Join(", ", type.GenericTypeArguments.Select(GetFullName));
+
+                var fullName = namespacedName + "<" + genericParameters + ">";
+
+                return fullName;
+            }
+
+            return type.FullName;
+        }
+
 
         private string GenerateGuidList(int count)
         {
