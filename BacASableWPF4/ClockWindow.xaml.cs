cebeabdfb8331873aca4f6970e628a446735421a
Matthieu Mourisson <mareek@gmail.com>
2021-04-13 21:35:30 +0200

    Ajout d'une fenêtre indiquant l'heure

---
+++ BacASableWPF4/ClockWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace BacASableWPF4
+{
+    /// <summary>
+    /// Interaction logic for ClockWindow.xaml
+    /// </summary>
+    public partial class ClockWindow : Window
+    {
+        private readonly DispatcherTimer _timer ;
+        public ClockWindow()
+        {
+            InitializeComponent();
+            _timer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromMilliseconds(50)
+            };
+            _timer.Tick +=  (_,_) => ClockLabel.Text = DateTime.Now.ToString("HH:mm:ss");
+            _timer.Start();
+        }
+    }
+}
